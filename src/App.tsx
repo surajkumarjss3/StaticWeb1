ffad45ec4fc822d6fbdb5b10094e380f7f0da5d4
SURAJ KUMAR SHETTIGAR <109427583+surajkumarjss3@users.noreply.github.com>
2023-04-24 12:08:39 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello from Static Web Apps
+          Hello this is Suraj
         </p>
         <a
           className="App-link"
