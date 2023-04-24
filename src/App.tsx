39a8be8f6723410bc10b87b20e0558f871351c18
SURAJ KUMAR SHETTIGAR <109427583+surajkumarjss3@users.noreply.github.com>
2023-04-24 12:47:49 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello this is Suraj
+          Hello this is Suraj Kumar Shettigar
         </p>
         <a
           className="App-link"
